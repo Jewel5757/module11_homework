4b85b8fdd88a1355d1d5128b47c6b3146de347fb
Tatyana Chayko <jewel5757@mail.ru>
2022-11-21 21:14:48 +0300

    пше

---
+++ index.js
@@ -0,0 +1,9 @@
+//Напишите функцию getPercents(percent, number), которая возвращает {percent} процентов от {number}.
+
+export function getPercents(percent, number) {
+  const result = number/100*percent;
+  return result;
+}
+
+
+
---
+++ index.test.js
@@ -0,0 +1,31 @@
+import {getPercents}  from './index.js';
+
+// Успешное выполнение функции
+
+test ('calculate percent from number(percent, number)', () => {
+	
+	expect(getPercents(50, 500)).toBe(250);
+  
+}); 
+
+//Неуспешное выполнение функции. Получено значение "300", хотя 50% от 400 = 200
+
+xtest ('calculate percent from number(percent, number)', () => {
+	
+	expect(getPercents(50, 400)).toBe(300);
+  
+}); 
+
+//unit-тесты
+
+describe('calculate percent from number(percent, number)', () => {
+	
+	it('function that counts percentages of a given number', () => {
+	  expect(getPercents(50, 100)).toBe(50);
+	  expect(getPercents(100,100)).toBe(100);
+	  expect(getPercents(20,200)).toBe(40);
+	});
+  }); 
+
+
+
