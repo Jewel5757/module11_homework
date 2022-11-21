1c6dc2e67f99577b9c7c5e462c0c52fbe40c4001
Tatyana Chayko <jewel5757@mail.ru>
2022-11-21 21:43:48 +0300

    add corner cases

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 //Напишите функцию getPercents(percent, number), которая возвращает {percent} процентов от {number}.
 
 export function getPercents(percent, number) {
-  const result = number/100*percent;
+  let result;
+  if ((!isNaN(percent))&&(!isNaN(number))){
+    result = number/100*percent;
+  } else {
+    result = 'введите число';
+  }
   return result;
 }
 
